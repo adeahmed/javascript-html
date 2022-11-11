cdfe7f89e3b2810a158ab514b26b2648c3072907
adeahmed <98258192+adeahmed@users.noreply.github.com>
2022-11-11 08:34:42 +0200

    Update Moduuli 04.js

--- Moduuli 04.js
+++ Moduuli 04.js
@@ -3,20 +3,18 @@ let r = Math.random()
 let nimi = prompt("anna nimi ")
 
 if (r < 0.25){
-    
-    document.write("" + "daredevil " + nimi )
+
+    document.body.textContent = "Darefevil" + nimi
+
 }
 else if (r > 0.25 && r < 0.5){
-    
-    document.write("" + "Slytherin " + nimi )
+
+    document.body.textContent = "slytherin " + nimi
 }
 else if((r > 0.5 && r < 0.75)){
-    
-    document.write("" + "Huffel epuff " + nimi )
+    document.body.textContent = "Hufflepuff " + nimi
 }
 else {
-
-    document.write("" + "Ravenclaw " + nimi )
+    document.body.textContent = "Ravenclaw " + nimi
 }
-
 
